442288409ca13b38c595256ea9b320427575342b
Artur Faruga <farugaa.training@gmail.com>
2017-10-29 16:57:07 +0100

    logging and exceptions

--- src/main/java/pl/akademiakodu/thymeleafproject/controller/BookController.java
+++ src/main/java/pl/akademiakodu/thymeleafproject/controller/BookController.java
@@ -1,5 +1,7 @@
 package pl.akademiakodu.thymeleafproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,34 +10,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.akademiakodu.thymeleafproject.model.Book;
-import pl.akademiakodu.thymeleafproject.model.BookCRUDRepository;
-import pl.akademiakodu.thymeleafproject.model.BookRepository;
 import pl.akademiakodu.thymeleafproject.service.BookService;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     @Autowired
     BookService bookService;
 
     //class mapping + method mapping "/book/showall"
     @GetMapping("/showall")
-    public String showAllBooks(ModelMap modelMap){
+    public String showAllBooks(ModelMap modelMap) {
         modelMap.addAttribute("book", bookService.findAll());
         return "book/showall";
     }
 
     @GetMapping("/add")
-    public String addBook(ModelMap modelMap){
+    public String addBook(ModelMap modelMap) {
         modelMap.addAttribute("book", new Book());
         modelMap.addAttribute("books", bookService.findAll());
         return "book/add";
     }
 
     @PostMapping("/add")
-    public String createBook(@ModelAttribute Book book){
-        bookService.save(book);
+    public String createBook(@ModelAttribute Book book) {
+        log.info("I'm creating book={}", book);
+        try {
+            bookService.save(book);
+        } catch (IllegalAccessException e) {
+            log.error("Saving book operation failed", e);
+        }
 
         return "redirect:/book/add";
     }
--- src/main/java/pl/akademiakodu/thymeleafproject/service/BookService.java
+++ src/main/java/pl/akademiakodu/thymeleafproject/service/BookService.java
@@ -1,23 +1,41 @@
 package pl.akademiakodu.thymeleafproject.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.akademiakodu.thymeleafproject.model.Book;
 import pl.akademiakodu.thymeleafproject.model.BookCRUDRepository;
+
+import java.util.Collections;
 
 
 //this is place for buisness logic
 @Service
 public class BookService {
 
+    private static final Logger log = LoggerFactory.getLogger(BookService.class);
+
     @Autowired
     BookCRUDRepository bookCRUDRepository;
 
     public Iterable<Book> findAll() {
-        return bookCRUDRepository.findAll();
+        log.info("User is trying to get all books");
+        try {
+            return bookCRUDRepository.findAll();
+        } catch (Exception e) {
+            log.error("Exception occured during call to database", e);
+            return Collections.emptyList();
+        }
     }
 
-    public Book save(Book book){
+    public Book save(Book book) throws IllegalAccessException {
+        log.info("User is trying to get all books");
+
+        if (book.getAuthor().equalsIgnoreCase("artur")) {
+            log.error("artur is not allowed to save book={}", book);
+            throw new IllegalAccessException("artur is not allowed to save book");
+        }
         return bookCRUDRepository.save(book);
     }
 }
