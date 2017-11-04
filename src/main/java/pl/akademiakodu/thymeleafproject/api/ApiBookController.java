70e518ab0bda9e95f929dd09fcde78c6c9fca725
Artur Faruga <farugaa.training@gmail.com>
2017-11-04 13:13:59 +0100

    restcontroller to controller

--- src/main/java/pl/akademiakodu/thymeleafproject/api/ApiBookController.java
+++ src/main/java/pl/akademiakodu/thymeleafproject/api/ApiBookController.java
@@ -1,11 +1,13 @@
 package pl.akademiakodu.thymeleafproject.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.akademiakodu.thymeleafproject.model.Book;
 import pl.akademiakodu.thymeleafproject.service.BookService;
@@ -13,13 +15,14 @@ import pl.akademiakodu.thymeleafproject.service.BookService;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class ApiBookController {
 
     @Autowired
     BookService bookService;
 
     @GetMapping(value = "/api/books" )
+    @ResponseBody
     public List<Book> getAllBooks() {
         List<Book> books = new ArrayList<>();
         for (Book iterBook : bookService.findAll()) {
@@ -29,12 +32,14 @@ public class ApiBookController {
     }
 
     @GetMapping(value = "/api/books/{id}" )
+    @ResponseBody //Jackson librar
     public Book getAllBooks(@PathVariable Long id) {
         Book book = bookService.fineOne(id);
-        return book;
+        return book; //serializcja do JSONA
     }
 
     @PostMapping(value = "api/books")
+    @ResponseBody           // deserializacja z JSONA
     public void saveBook(@RequestBody Book book) throws IllegalAccessException {
         bookService.save(book);
     }
